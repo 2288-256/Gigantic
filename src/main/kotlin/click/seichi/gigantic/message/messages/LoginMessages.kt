b4be64024683fa2a6916c8835b08c3833aeab95c
2288 <74705442+2288-256@users.noreply.github.com>
2024-12-19 00:56:36 +0900

    リンク修正

--- src/main/kotlin/click/seichi/gigantic/message/messages/LoginMessages.kt
+++ src/main/kotlin/click/seichi/gigantic/message/messages/LoginMessages.kt
@@ -15,150 +15,160 @@ import java.util.*
 object LoginMessages {
 
     val LOGIN_CHAT = LinedChatMessage(ChatMessageProtocol.CHAT,
-            LocalizedText(
-                    Locale.JAPANESE to "${ChatColor.WHITE}" +
-                            (1..53).joinToString("") { "-" } +
-                            LinedChatMessage.NEW_LINE_SYMBOL +
-                            "${ChatColor.LIGHT_PURPLE}${ChatColor.BOLD}" +
-                            "非公式整地鯖(春)" +
-                            "${ChatColor.WHITE}" +
-                            "へようこそ!" +
-                            LinedChatMessage.NEW_LINE_SYMBOL +
-                            "${ChatColor.AQUA}${ChatColor.BOLD}" +
+        LocalizedText(
+            Locale.JAPANESE to "${ChatColor.WHITE}" +
+                    (1..53).joinToString("") { "-" } +
+                    LinedChatMessage.NEW_LINE_SYMBOL +
+                    "${ChatColor.LIGHT_PURPLE}${ChatColor.BOLD}" +
+                    "非公式整地鯖(春)" +
+                    "${ChatColor.WHITE}" +
+                    "へようこそ!" +
+                    LinedChatMessage.NEW_LINE_SYMBOL +
+                    "${ChatColor.AQUA}${ChatColor.BOLD}" +
                             "このサーバーは整地鯖(春)をGPL-3.0 licenseの条件でForkした"+
-                            LinedChatMessage.NEW_LINE_SYMBOL +
+                    LinedChatMessage.NEW_LINE_SYMBOL +
                             "${ChatColor.RED}${ChatColor.BOLD}"+
-                            "非公式サーバーです。" +
-                            LinedChatMessage.NEW_LINE_SYMBOL +
-                            "${ChatColor.WHITE}" +
-                            "ソースコード: " +
-                            "${ChatColor.YELLOW}" +
-                            "https://github.com/2288-256/Unofficial-Gigantic" +
-                            LinedChatMessage.NEW_LINE_SYMBOL +
-                            "${ChatColor.WHITE}" +
-                            "実装予定の機能・発生中の不具合: " +
-                            "${ChatColor.YELLOW}" +
-                            "https://github.com/2288-256/Unofficial-Gigantic/issues" +
-                            LinedChatMessage.NEW_LINE_SYMBOL +
-                            "${ChatColor.WHITE}" +
-                            "リリースノート: " +
-                            "${ChatColor.YELLOW}" +
-                            "https://github.com/2288-256/Unofficial-Gigantic/releases" +
-                            LinedChatMessage.NEW_LINE_SYMBOL +
-                            "${ChatColor.WHITE}" +
-                            "お問い合わせフォーム(機能追加の要望・通報): " +
-                            "${ChatColor.YELLOW}" +
-                            "https://forms.gle/PMMiXbGrRATTVReP9" +
-                            LinedChatMessage.NEW_LINE_SYMBOL +
-                            LinedChatMessage.NEW_LINE_SYMBOL +
-                            "${ChatColor.WHITE}" +
-                            "※" +
-                            "${ChatColor.LIGHT_PURPLE}${ChatColor.BOLD}" +
-                            "本サーバー 非公式整地鯖(春)" +
-                            "${ChatColor.WHITE}" +
-                            "に関するお問い合わせを" +
-                            LinedChatMessage.NEW_LINE_SYMBOL +
-                            "${ChatColor.GREEN}${ChatColor.BOLD}" +
-                            "ギガンティック整地鯖・整地鯖(春)" +
-                            "${ChatColor.WHITE}" +
-                            "運営チームに" +
-                            "${ChatColor.RED}${ChatColor.BOLD}"+
-                            "行わないでください" +
-                            LinedChatMessage.NEW_LINE_SYMBOL +
-                            "${ChatColor.WHITE}" +
-                            (1..53).joinToString("") { "-" }
+                    "非公式サーバーです。" +
+                    LinedChatMessage.NEW_LINE_SYMBOL +
+                    "${ChatColor.WHITE}" +
+                    "ソースコード: " +
+                    "${ChatColor.YELLOW}" +
+                    "https://s.seichi-haru.pgw.jp/github" +
+                    LinedChatMessage.NEW_LINE_SYMBOL +
+                    "${ChatColor.WHITE}" +
+                    "実装予定の機能・発生中の不具合: " +
+                    "${ChatColor.YELLOW}" +
+                    "https://s.seichi-haru.pgw.jp/issues" +
+                    LinedChatMessage.NEW_LINE_SYMBOL +
+                    "${ChatColor.WHITE}" +
+                    "リリースノート: " +
+                    "${ChatColor.YELLOW}" +
+                    "https://s.seichi-haru.pgw.jp/releases" +
+                    LinedChatMessage.NEW_LINE_SYMBOL +
+                    "${ChatColor.WHITE}" +
+                    "お問い合わせフォーム(機能追加の要望・通報): " +
+                    "${ChatColor.YELLOW}" +
+                    "https://forms.gle/PMMiXbGrRATTVReP9" +
+                    LinedChatMessage.NEW_LINE_SYMBOL +
+                    "${ChatColor.WHITE}" +
+                    "Discord: " +
+                    "${ChatColor.YELLOW}" +
+                    "https://s.seichi-haru.pgw.jp/discord" +
+                    LinedChatMessage.NEW_LINE_SYMBOL +
+                    "${ChatColor.WHITE}" +
+                    "WebMap: " +
+                    "${ChatColor.YELLOW}" +
+                    "https://map.seichi-haru.pgw.jp" +
+                    LinedChatMessage.NEW_LINE_SYMBOL +
+                    LinedChatMessage.NEW_LINE_SYMBOL +
+                    "${ChatColor.WHITE}" +
+                    "※" +
+                    "${ChatColor.LIGHT_PURPLE}${ChatColor.BOLD}" +
+                    "本サーバー 非公式整地鯖(春)" +
+                    "${ChatColor.WHITE}" +
+                    "に関するお問い合わせを" +
+                    LinedChatMessage.NEW_LINE_SYMBOL +
+                    "${ChatColor.GREEN}${ChatColor.BOLD}" +
+                    "ギガンティック整地鯖・整地鯖(春)" +
+                    "${ChatColor.WHITE}" +
+                    "運営チームに" +
+                    "${ChatColor.RED}${ChatColor.BOLD}"+
+                    "行わないでください" +
+                    LinedChatMessage.NEW_LINE_SYMBOL +
+                    "${ChatColor.WHITE}" +
+                    (1..53).joinToString("") { "-" }
 
-            ))
+        ))
 
     val LOGIN_TITLE = TitleMessage(
-            title = LocalizedText(
-                    Locale.JAPANESE to "${ChatColor.LIGHT_PURPLE}" +
-                            "非公式整地鯖(春)"
+        title = LocalizedText(
+            Locale.JAPANESE to "${ChatColor.LIGHT_PURPLE}" +
+                    "非公式整地鯖(春)"
             ), subTitle = null)
 
     val EVENT_SAKURA = LinedChatMessage(ChatMessageProtocol.CHAT, LocalizedText(
-            Locale.JAPANESE to "${ChatColor.LIGHT_PURPLE}" +
-                    (1..53).joinToString("") { "-" } +
-                    LinedChatMessage.NEW_LINE_SYMBOL +
-                    "${ChatColor.WHITE}" +
-                    "初イベント " +
-                    "${ChatColor.LIGHT_PURPLE}${ChatColor.BOLD}" +
-                    "春の整地祭り" +
-                    "${ChatColor.WHITE}" +
-                    " を開催中!!" +
-                    LinedChatMessage.NEW_LINE_SYMBOL +
-                    "${ChatColor.WHITE}" +
-                    "期間限定意志" +
-                    "${ChatColor.LIGHT_PURPLE}${ChatColor.BOLD}" +
-                    " 桜の意志 " +
-                    "${ChatColor.WHITE}" +
-                    "を獲得して、特別なレリックを手に入れよう!!" +
-                    LinedChatMessage.NEW_LINE_SYMBOL +
-                    "${ChatColor.LIGHT_PURPLE}" +
-                    (1..53).joinToString("") { "-" }
+        Locale.JAPANESE to "${ChatColor.LIGHT_PURPLE}" +
+                (1..53).joinToString("") { "-" } +
+                LinedChatMessage.NEW_LINE_SYMBOL +
+                "${ChatColor.WHITE}" +
+                "初イベント " +
+                "${ChatColor.LIGHT_PURPLE}${ChatColor.BOLD}" +
+                "春の整地祭り" +
+                "${ChatColor.WHITE}" +
+                " を開催中!!" +
+                LinedChatMessage.NEW_LINE_SYMBOL +
+                "${ChatColor.WHITE}" +
+                "期間限定意志" +
+                "${ChatColor.LIGHT_PURPLE}${ChatColor.BOLD}" +
+                " 桜の意志 " +
+                "${ChatColor.WHITE}" +
+                "を獲得して、特別なレリックを手に入れよう!!" +
+                LinedChatMessage.NEW_LINE_SYMBOL +
+                "${ChatColor.LIGHT_PURPLE}" +
+                (1..53).joinToString("") { "-" }
     ), 0L)
 
     val EVENT_MIO = LinedChatMessage(ChatMessageProtocol.CHAT, LocalizedText(
-            Locale.JAPANESE to "${ChatColor.AQUA}" +
-                    (1..53).joinToString("") { "-" } +
-                    LinedChatMessage.NEW_LINE_SYMBOL +
-                    "${ChatColor.WHITE}" +
-                    "${ChatColor.AQUA}${ChatColor.BOLD}" +
-                    "夏の整地祭り" +
-                    "${ChatColor.WHITE}" +
-                    " を開催中!!" +
-                    LinedChatMessage.NEW_LINE_SYMBOL +
-                    "${ChatColor.WHITE}" +
-                    "期間限定意志" +
-                    "${ChatColor.AQUA}${ChatColor.BOLD}" +
-                    " 澪の意志 " +
-                    "${ChatColor.WHITE}" +
-                    "を獲得して、特別なレリックを手に入れよう!!" +
-                    LinedChatMessage.NEW_LINE_SYMBOL +
-                    "${ChatColor.AQUA}" +
-                    (1..53).joinToString("") { "-" }
+        Locale.JAPANESE to "${ChatColor.AQUA}" +
+                (1..53).joinToString("") { "-" } +
+                LinedChatMessage.NEW_LINE_SYMBOL +
+                "${ChatColor.WHITE}" +
+                "${ChatColor.AQUA}${ChatColor.BOLD}" +
+                "夏の整地祭り" +
+                "${ChatColor.WHITE}" +
+                " を開催中!!" +
+                LinedChatMessage.NEW_LINE_SYMBOL +
+                "${ChatColor.WHITE}" +
+                "期間限定意志" +
+                "${ChatColor.AQUA}${ChatColor.BOLD}" +
+                " 澪の意志 " +
+                "${ChatColor.WHITE}" +
+                "を獲得して、特別なレリックを手に入れよう!!" +
+                LinedChatMessage.NEW_LINE_SYMBOL +
+                "${ChatColor.AQUA}" +
+                (1..53).joinToString("") { "-" }
     ), 0L)
 
     val EVENT_KAEDE = LinedChatMessage(ChatMessageProtocol.CHAT, LocalizedText(
-            Locale.JAPANESE to "${ChatColor.GOLD}" +
-                    (1..53).joinToString("") { "-" } +
-                    LinedChatMessage.NEW_LINE_SYMBOL +
-                    "${ChatColor.WHITE}" +
-                    "${ChatColor.GOLD}${ChatColor.BOLD}" +
-                    "秋の整地祭り" +
-                    "${ChatColor.WHITE}" +
-                    " を開催中!!" +
-                    LinedChatMessage.NEW_LINE_SYMBOL +
-                    "${ChatColor.WHITE}" +
-                    "期間限定意志" +
-                    "${ChatColor.GOLD}${ChatColor.BOLD}" +
-                    " 楓の意志 " +
-                    "${ChatColor.WHITE}" +
-                    "を獲得して、特別なレリックを手に入れよう!!" +
-                    LinedChatMessage.NEW_LINE_SYMBOL +
-                    "${ChatColor.GOLD}" +
-                    (1..53).joinToString("") { "-" }
+        Locale.JAPANESE to "${ChatColor.GOLD}" +
+                (1..53).joinToString("") { "-" } +
+                LinedChatMessage.NEW_LINE_SYMBOL +
+                "${ChatColor.WHITE}" +
+                "${ChatColor.GOLD}${ChatColor.BOLD}" +
+                "秋の整地祭り" +
+                "${ChatColor.WHITE}" +
+                " を開催中!!" +
+                LinedChatMessage.NEW_LINE_SYMBOL +
+                "${ChatColor.WHITE}" +
+                "期間限定意志" +
+                "${ChatColor.GOLD}${ChatColor.BOLD}" +
+                " 楓の意志 " +
+                "${ChatColor.WHITE}" +
+                "を獲得して、特別なレリックを手に入れよう!!" +
+                LinedChatMessage.NEW_LINE_SYMBOL +
+                "${ChatColor.GOLD}" +
+                (1..53).joinToString("") { "-" }
     ), 0L)
 
     val EVENT_REI = LinedChatMessage(ChatMessageProtocol.CHAT, LocalizedText(
-            Locale.JAPANESE to "${ChatColor.AQUA}" +
-                    (1..53).joinToString("") { "-" } +
-                    LinedChatMessage.NEW_LINE_SYMBOL +
-                    "${ChatColor.WHITE}" +
-                    "${ChatColor.GOLD}${ChatColor.BOLD}" +
-                    "冬の整地祭り" +
-                    "${ChatColor.WHITE}" +
-                    " を開催中!!" +
-                    LinedChatMessage.NEW_LINE_SYMBOL +
-                    "${ChatColor.WHITE}" +
-                    "期間限定意志" +
-                    "${ChatColor.AQUA}${ChatColor.BOLD}" +
-                    " 玲の意志 " +
-                    "${ChatColor.WHITE}" +
-                    "を獲得して、特別なレリックを手に入れよう!!" +
-                    LinedChatMessage.NEW_LINE_SYMBOL +
-                    "${ChatColor.AQUA}" +
-                    (1..53).joinToString("") { "-" }
+        Locale.JAPANESE to "${ChatColor.AQUA}" +
+                (1..53).joinToString("") { "-" } +
+                LinedChatMessage.NEW_LINE_SYMBOL +
+                "${ChatColor.WHITE}" +
+                "${ChatColor.GOLD}${ChatColor.BOLD}" +
+                "冬の整地祭り" +
+                "${ChatColor.WHITE}" +
+                " を開催中!!" +
+                LinedChatMessage.NEW_LINE_SYMBOL +
+                "${ChatColor.WHITE}" +
+                "期間限定意志" +
+                "${ChatColor.AQUA}${ChatColor.BOLD}" +
+                " 玲の意志 " +
+                "${ChatColor.WHITE}" +
+                "を獲得して、特別なレリックを手に入れよう!!" +
+                LinedChatMessage.NEW_LINE_SYMBOL +
+                "${ChatColor.AQUA}" +
+                (1..53).joinToString("") { "-" }
     ), 0L)
 
     val EVENT_JMS_KING = LinedChatMessage(ChatMessageProtocol.CHAT, LocalizedText(
