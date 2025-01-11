ae23165cb3ec37e2127c8da439238407e7f14b5e
kuroma6666 <kuroma.creator@gmail.com>
2025-01-12 00:37:41 +0900

    chore: `seichi.click network`の整地鯖春のDisplayNameを追加

---
+++ src/main/kotlin/click/seichi/gigantic/message/messages/menu/SpecialThanksMenuMessages.kt
@@ -0,0 +1,14 @@
+package click.seichi.gigantic.message.messages.menu
+
+import click.seichi.gigantic.message.LocalizedText
+import org.bukkit.ChatColor
+import java.util.*
+
+object SpecialThanksMenuMessages {
+
+    val OFFICAL_SPECIAL_THANKS_DISPLAY = 
+        LocalizedText(
+            Locale.JAPANESE to "seichi.click network 整地鯖(春)"
+        )
+
+}
