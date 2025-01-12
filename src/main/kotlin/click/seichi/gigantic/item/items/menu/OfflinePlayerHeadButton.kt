8541651b7e705d7fd17d343b08646982448d9640
kuroma6666 <kuroma.creator@gmail.com>
2025-01-12 23:25:42 +0900

    fix: Loreを設定するため、`OfflinePlayerHeadButton`クラスにOptional引数`lore`を追加

--- src/main/kotlin/click/seichi/gigantic/item/items/menu/OfflinePlayerHeadButton.kt
+++ src/main/kotlin/click/seichi/gigantic/item/items/menu/OfflinePlayerHeadButton.kt
@@ -1,8 +1,10 @@
 package click.seichi.gigantic.item.items.menu
 
 import click.seichi.gigantic.extension.setDisplayName
+import click.seichi.gigantic.extension.setLore
 import click.seichi.gigantic.head.Head
 import click.seichi.gigantic.item.Button
+import org.bukkit.ChatColor
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import java.util.*
@@ -12,12 +14,16 @@ import java.util.*
  */
 class OfflinePlayerHeadButton(
         private val uniqueId: UUID,
-        private val name: String
+        private val name: String,
+        private val lore: String? = null
 ) : Button {
 
     override fun toShownItemStack(player: Player): ItemStack? {
         return Head.getOfflinePlayerHead(uniqueId).apply {
             setDisplayName(name)
+            if(!lore.isNullOrEmpty()){
+                setLore("${ChatColor.GRAY}"+lore)
+            }
         }
     }
 
